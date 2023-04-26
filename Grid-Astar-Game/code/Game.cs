6ba2dd3412c7b8f13256b12ae36fd12564792260
yuberee <59583743+yuberee@users.noreply.github.com>
2023-04-26 22:07:21 +0200

    Remove grid generation in game's code, use HammerGrid instead

--- Grid-Astar-Game/code/Game.cs
+++ Grid-Astar-Game/code/Game.cs
@@ -12,18 +12,7 @@ namespace GridAStarNPC;
 
 public partial class GridGame : GameManager
 {
-	public GridGame()
-	{
-		/*
-		if ( Game.IsServer ) // Try loading the Grid on the server, if it's not found it will create it in the PostLevelLoaded method
-			GridAStar.Grid.Load();
-		else
-		{
-			if ( GridAStar.Grid.Load().Result == null ) // Try loading the Grid on the client, else it creates a new one
-				GridAStar.Grid.Create( Vector3.Zero, Game.PhysicsWorld.Body.GetBounds(), new Rotation() );
-		}*/
-
-	}
+	public GridGame(){}
 
 	public override void ClientJoined( IClient client )
 	{
@@ -42,15 +31,5 @@ public partial class GridGame : GameManager
 			tx.Position = tx.Position + Vector3.Up * 50.0f;
 			pawn.Transform = tx;
 		}
-	}
-
-	public override void PostLevelLoaded()
-	{
-		base.PostLevelLoaded();
-
-		/*if ( Game.IsServer )
-			if ( !GridAStar.Grid.Exists() )
-				GridAStar.Grid.Create( Vector3.Zero, Game.PhysicsWorld.Body.GetBounds(), new Rotation() ); // If no main grid was created before, create one now
-		*/
 	}
 }
