e3c80a172f622b1a44d1e3f9160c220a0fd6ddde
yuberee <59583743+yuberee@users.noreply.github.com>
2023-04-30 15:00:05 +0200

    GridBlocker works by assigning grid first

--- Grid-Astar-Library/code/Hammer/GridBlockerBox.cs
+++ Grid-Astar-Library/code/Hammer/GridBlockerBox.cs
@@ -9,17 +9,23 @@ namespace GridAStar;
 [Display( Name = "Grid Blocker Box", GroupName = "Navigation", Description = "Define an box where all cells inside are occupied (Can be rotated)" )]
 public partial class GridBlockerBox : ModelEntity
 {
+	[Net, Property, Description( "Which grid is affected" )]
+	public string GridIdentifier { get; set; } = "main";
 	[Net, Property, Description( "Expand the blocker before checking, this should be like your grid's width clearance" )]
 	public float WidthClearance { get; set; } = GridSettings.DEFAULT_WIDTH_CLEARANCE;
+	public Grid CurrentGrid { get; set; }
 	public BBox BoundsWithClearance => new BBox( CollisionBounds.Mins - WidthClearance, CollisionBounds.Maxs + WidthClearance );
 	public BBox RotatedBounds => CollisionBounds.GetRotatedBounds( Rotation );
 	public BBox WorldBounds => RotatedBounds.Translate( Position );
-	public List<Grid> OverlappingGrids { get; set;} = new();
 	public HashSet<Cell> OverlappingCells { get; set; } = new();
 	public bool IsInsideBounds( Vector3 point ) => BoundsWithClearance.IsRotatedPointWithinBounds( Position, point, Rotation );
 
-	public GridBlockerBox()
+	public GridBlockerBox() { }
+
+	public GridBlockerBox( Grid grid ) : base()
 	{
+		CurrentGrid = grid;
+		GridIdentifier = grid.Identifier;
 	}
 
 	public override void Spawn()
@@ -28,34 +34,34 @@ public partial class GridBlockerBox : ModelEntity
 
 		EnableDrawing = false;
 
-		FindAndApplyAll();
+		SearchForGrid();
+		Apply();
 	}
 
-	public void FindOverlappingGrids()
+	public void SearchForGrid()
 	{
-		foreach ( var grid in Grid.Grids )
-			if ( grid.Value.WorldBounds.Overlaps( WorldBounds ) )
-				OverlappingGrids.Add( grid.Value );
+		if ( CurrentGrid == null || CurrentGrid.Identifier != GridIdentifier )
+			if ( Grid.Grids.ContainsKey( GridIdentifier ) )
+				CurrentGrid = Grid.Grids[GridIdentifier];
 	}
 
-	public void ApplyOccupied( Grid grid )
+	public void Apply()
 	{
-		foreach ( var cellStack in grid.Cells )
-			foreach ( var cell in cellStack.Value )
-				if ( IsInsideBounds( cell.Position ) )
-				{
-					OverlappingCells.Add( cell );
-					cell.Occupied = true;
-				}
+		if ( CurrentGrid != null )
+			foreach ( var cellStack in CurrentGrid.Cells )
+				foreach ( var cell in cellStack.Value )
+					if ( IsInsideBounds( cell.Position ) )
+					{
+						OverlappingCells.Add( cell );
+						cell.Occupied = true;
+					}
 	}
 
 	[Grid.LoadedAll]
-	public void FindAndApplyAll()
+	public void ApplyAll()
 	{
-		FindOverlappingGrids();
-
-		foreach ( var grid in OverlappingGrids )
-			ApplyOccupied( grid );
+		SearchForGrid();
+		Apply();
 	}
 
 }
