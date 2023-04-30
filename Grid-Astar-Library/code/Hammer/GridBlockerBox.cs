69a86bdab877132a7c3acf2c4a61ecd833932b20
yuberee <59583743+yuberee@users.noreply.github.com>
2023-04-30 12:34:18 +0200

    Implement GridBlocker

--- Grid-Astar-Library/code/Hammer/GridBlockerBox.cs
+++ Grid-Astar-Library/code/Hammer/GridBlockerBox.cs
@@ -1,4 +1,5 @@
 ﻿using Editor;
+using Sandbox;
 using System;
 using System.ComponentModel.DataAnnotations;
 
@@ -10,6 +11,10 @@ public partial class GridBlockerBox : ModelEntity
 {
 	[Net, Property, Description( "Expand the blocker before checking, this should be like your grid's width clearance" )]
 	public float WidthClearance { get; set; } = GridSettings.DEFAULT_WIDTH_CLEARANCE;
+	public BBox RotatedBounds => CollisionBounds.GetRotatedBounds( Rotation );
+	public BBox WorldBounds => RotatedBounds.Translate( Position );
+	public List<Grid> OverlappingGrids { get; set;} = new List<Grid>();
+	public bool IsInsideBounds( Vector3 point ) => CollisionBounds.IsRotatedPointWithinBounds( Position, point, Rotation );
 
 	public GridBlockerBox()
 	{
@@ -22,10 +27,28 @@ public partial class GridBlockerBox : ModelEntity
 		EnableDrawing = false;
 	}
 
+	public void FindOverlappingGrids()
+	{
+		foreach ( var grid in Grid.Grids )
+			if ( grid.Value.WorldBounds.Overlaps( WorldBounds ) )
+				OverlappingGrids.Add( grid.Value );
+	}
+
+	public void ApplyOccupied( Grid grid )
+	{
+		foreach ( var cellStack in grid.Cells )
+			foreach ( var cell in cellStack.Value )
+				if ( IsInsideBounds( cell.Position ) )
+					cell.Occupied = true;
+	}
+
 	[Grid.LoadedAll]
 	public void CheckOverlap()
 	{
-		Log.Error( "LOADED ALL" );
+		FindOverlappingGrids();
+
+		foreach ( var grid in OverlappingGrids )
+			ApplyOccupied( grid );
 	}
 
 }
