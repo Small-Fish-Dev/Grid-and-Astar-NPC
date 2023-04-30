d2d1681f622fffc834b5ddb8d0b653b79f2fe6f0
yuberee <59583743+yuberee@users.noreply.github.com>
2023-04-30 12:17:28 +0200

    Grid.LoadedAll event

---
+++ Grid-Astar-Library/code/Grid/Grid.Events.cs
@@ -0,0 +1,11 @@
+﻿namespace GridAStar;
+
+public partial class Grid
+{
+	public const string LoadedAll = "grid.loadedall";
+
+	public class LoadedAllAttribute : EventAttribute
+	{
+		public LoadedAllAttribute() : base( LoadedAll ) { }
+	}
+}
---
+++ Grid-Astar-Library/code/Hammer/GridBlockerBox.cs
@@ -0,0 +1,31 @@
+﻿using Editor;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GridAStar;
+
+[HammerEntity, Solid, AutoApplyMaterial( "materials/no_grid.vmat" )]
+[Display( Name = "Grid Blocker Box", GroupName = "Navigation", Description = "Define an box where all cells inside are occupied (Can be rotated)" )]
+public partial class GridBlockerBox : ModelEntity
+{
+	[Net, Property, Description( "Expand the blocker before checking, this should be like your grid's width clearance" )]
+	public float WidthClearance { get; set; } = GridSettings.DEFAULT_WIDTH_CLEARANCE;
+
+	public GridBlockerBox()
+	{
+	}
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		EnableDrawing = false;
+	}
+
+	[Grid.LoadedAll]
+	public void CheckOverlap()
+	{
+		Log.Error( "LOADED ALL" );
+	}
+
+}
