5d91ea18c7ae4916bafa663ed38b804bd8c4c90a
yuberee <59583743+yuberee@users.noreply.github.com>
2023-06-06 18:15:26 +0200

    Move classes into their own files

--- Grid-Astar-Library/code/Grid/AStarPathBuilder.cs
+++ Grid-Astar-Library/code/Grid/AStarPathBuilder.cs
@@ -2,58 +2,6 @@
 
 namespace GridAStar;
 
-public struct AStarPath
-{
-	public AStarPathBuilder Settings { get; internal set; }
-	public List<Cell> Cells { get; set; }
-	public Grid Grid => Settings.Grid;
-	public int Count => Cells.Count();
-	public bool IsEmpty => Cells == null || Count == 0;
-
-	public AStarPath() { }
-
-	public AStarPath( AStarPathBuilder builder, List<Cell> cells ) : this()
-	{
-		Settings = builder;
-		Cells = cells;
-	}
-
-	public static AStarPath From( AStarPathBuilder builder, List<Cell> cells ) => new AStarPath( builder, cells );
-
-	public static AStarPath Empty() => new AStarPath();
-
-	/// <summary>
-	/// Simplify the path by iterating over line of sights between the given segment size, joining them if valid
-	/// </summary>
-	/// <param name="segmentAmounts"></param>
-	/// <param name="iterations"></param>
-	/// <returns></returns>
-	public void Simplify( int segmentAmounts = 2, int iterations = 8 )
-	{
-		for ( int iteration = 0; iteration < iterations; iteration++ )
-		{
-			var segmentStart = 0;
-			var segmentEnd = Math.Min( segmentAmounts, Count - 1 );
-
-			while ( Count > 2 && segmentEnd < Count - 1 )
-			{
-				var currentCell = Cells[segmentStart];
-				var furtherCell = Cells[segmentEnd];
-
-				if ( Settings.Grid.LineOfSight( currentCell, furtherCell, Settings.PathCreator ) )
-					for ( int toDelete = segmentStart + 1; toDelete < segmentEnd; toDelete++ )
-						Cells.RemoveAt( toDelete );
-
-				if ( segmentEnd == Count - 1 )
-					break;
-
-				segmentStart++;
-				segmentEnd = Math.Min( segmentStart + segmentAmounts, Count - 1 );
-			}
-		}
-	}
-}
-
 public struct AStarPathBuilder
 {
 	public Grid Grid { get; private set; } = null;
---
+++ Grid-Astar-Library/code/Grid/GridBuilder.cs
@@ -0,0 +1,328 @@
+﻿namespace GridAStar;
+
+public struct GridBuilder
+{
+	public string Identifier { get; private set; } = "main";
+	public float StandableAngle { get; private set; } = GridSettings.DEFAULT_STANDABLE_ANGLE;
+	public float StepSize { get; private set; } = GridSettings.DEFAULT_STEP_SIZE;
+	public float CellSize { get; private set; } = GridSettings.DEFAULT_CELL_SIZE;
+	public float HeightClearance { get; private set; } = GridSettings.DEFAULT_HEIGHT_CLEARANCE;
+	public float WidthClearance { get; private set; } = GridSettings.DEFAULT_WIDTH_CLEARANCE;
+	public bool GridPerfect { get; private set; } = GridSettings.DEFAULT_GRID_PERFECT;
+	public bool WorldOnly { get; private set; } = GridSettings.DEFAULT_WORLD_ONLY;
+	public bool AxisAligned { get; private set; } = false;
+	public bool CylinderShaped { get; private set; } = false;
+	public List<string> TagsToInclude { get; private set; } = new() { "solid" };
+	public List<string> TagsToExclude { get; private set; } = new() { "player" };
+	public Vector3 Position { get; private set; } = new();
+	public BBox Bounds { get; private set; } = new();
+	public Rotation Rotation { get; set; } = new();
+
+	public GridBuilder()
+	{
+		var map = Game.WorldEntity.IsValid() ? Game.WorldEntity : null;
+		var mapBounds = map == null ? new BBox( 0 ) : Game.WorldEntity.WorldSpaceBounds;
+		Position = mapBounds.Center;
+		Bounds = mapBounds;
+	}
+
+	/// <summary>
+	///  By default the identifier is "main", which makes it useable with Grid.Main
+	/// </summary>
+	/// <param name="identifier"></param>
+	public GridBuilder( string identifier ) : this()
+	{
+		Identifier = identifier;
+
+		var map = Game.WorldEntity.IsValid() ? Game.WorldEntity : null;
+		var mapBounds = map == null ? new BBox(0) : Game.WorldEntity.WorldSpaceBounds;
+		Position = mapBounds.Center;
+		Bounds = mapBounds;
+	}
+
+	/// <summary>
+	/// How steep the terrain can be before a cell doesn't get generated on it
+	/// </summary>
+	/// <param name="standableAngle"></param>
+	/// <returns></returns>
+	public GridBuilder WithStandableAngle( float standableAngle )
+	{
+		StandableAngle = standableAngle;
+		return this;
+	}
+
+	/// <summary>
+	/// How tall steps can be to be considered walkable (Doesn't work if GridPerfect)
+	/// </summary>
+	/// <param name="stepSize"></param>
+	/// <returns></returns>
+	public GridBuilder WithStepSize( float stepSize )
+	{
+		if ( !GridPerfect )
+			StepSize = stepSize;
+		return this;
+	}
+
+	/// <summary>
+	/// How big the cells are generated
+	/// </summary>
+	/// <param name="cellSize"></param>
+	/// <returns></returns>
+	public GridBuilder WithCellSize( float cellSize )
+	{
+		CellSize = cellSize;
+		return this;
+	}
+
+	/// <summary>
+	/// Minimum vertical space for a cell to be generated
+	/// </summary>
+	/// <param name="heightClearance"></param>
+	/// <returns></returns>
+	public GridBuilder WithHeightClearance( float heightClearance )
+	{
+		HeightClearance = heightClearance;
+		return this;
+	}
+
+	/// <summary>
+	/// Minimum horizontal space for a cell to be generate
+	/// </summary>
+	/// <param name="widthClearance"></param>
+	/// <returns></returns>
+	public GridBuilder WithWidthClearance( float widthClearance )
+	{
+		WidthClearance = widthClearance;
+		return this;
+	}
+
+	/// <summary>
+	/// Cells will be generated with some clearance for grid-perfect terrain (Think of voxels), this isn't compatible with stairs so use sloped geometry instead.
+	/// </summary>
+	/// <returns></returns>
+	public GridBuilder WithGridPerfect()
+	{
+		GridPerfect = true;
+		StepSize = 0f;
+		return this;
+	}
+
+	/// <summary>
+	/// Ignore entities, only hit create cells on the world
+	/// </summary>
+	/// <returns></returns>
+	public GridBuilder WithWorldOnly()
+	{
+		WorldOnly = true;
+		return this;
+	}
+
+	/// <summary>
+	/// Only hit entities with the following tags ("solid" is included by default, you can exclude it with WithoutTags)
+	/// </summary>
+	/// <returns></returns>
+	public GridBuilder WithTags( params string[] tags )
+	{
+		foreach ( var tag in tags )
+		{
+			if ( !TagsToInclude.Contains( tag ) )
+				TagsToInclude.Add( tag );
+			if ( TagsToExclude.Contains( tag ) )
+				TagsToExclude.Remove( tag );
+		}
+		return this;
+	}
+
+	/// <summary>
+	/// Only hit entities without the following tags ("player" is included by default, you can include it with WithTags)
+	/// </summary>
+	/// <returns></returns>
+	public GridBuilder WithoutTags( params string[] tags )
+	{
+		foreach ( var tag in tags )
+		{
+			if ( !TagsToExclude.Contains( tag ) )
+				TagsToExclude.Add( tag );
+			if ( TagsToInclude.Contains( tag ) )
+				TagsToInclude.Remove( tag );
+		}
+		return this;
+	}
+
+	/// <summary>
+	/// Set the position and bounds of the grid, by default it covers the entire map
+	/// </summary>
+	/// <param name="position"></param>
+	/// <param name="bounds"></param>
+	/// <returns></returns>
+	public GridBuilder WithBounds( Vector3 position, BBox bounds )
+	{
+		Position = position;
+		Bounds = bounds;
+		return this;
+	}
+
+	/// <summary>
+	/// Set the position, bounds, and rotation of the grid, by default it covers the entire map and is unrotated
+	/// </summary>
+	/// <param name="position"></param>
+	/// <param name="bounds"></param>
+	/// <param name="rotation"></param>
+	/// <returns></returns>
+	public GridBuilder WithBounds( Vector3 position, BBox bounds, Rotation rotation )
+	{
+		Position = position;
+		Bounds = bounds;
+		Rotation = rotation;
+		return this;
+	}
+
+	/// <summary>
+	/// Rotated the grid's bounds
+	/// </summary>
+	/// <param name="rotation"></param>
+	/// <returns></returns>
+	public GridBuilder WithRotation( Rotation rotation )
+	{
+		Rotation = rotation;
+		return this;
+	}
+
+	/// <summary>
+	/// The cells will be generated following this grid's rotation instead of the world's
+	/// </summary>
+	/// <returns></returns>
+	public GridBuilder WithAxisAligned()
+	{
+		AxisAligned = true;
+		return this;
+	}
+
+	/// <summary>
+	/// Generate the cells inside of a cylinder that fits the bounds instead of a cube (Squished if the bounds are)
+	/// </summary>
+	/// <returns></returns>
+	public GridBuilder WithCylinderShaped()
+	{
+		CylinderShaped = true;
+		return this;
+	}
+
+
+	/// <summary>
+	/// Creates a new grid with the settings given
+	/// <returns></returns>
+	public async Task<Grid> Create()
+	{
+		Stopwatch totalWatch = new Stopwatch();
+		totalWatch.Start();
+
+		var currentGrid = new Grid( this );
+
+		Log.Info( $"{(Game.IsServer ? "[Server]" : "[Client]")} Creating grid {currentGrid.Identifier}" );
+
+		var rotatedBounds = currentGrid.RotatedBounds;
+		var worldBounds = currentGrid.WorldBounds;
+
+		var minimumGrid = rotatedBounds.Mins.ToIntVector2( currentGrid.CellSize );
+		var maximumGrid = rotatedBounds.Maxs.ToIntVector2( currentGrid.CellSize );
+		var totalColumns = maximumGrid.y - minimumGrid.y;
+		var totalRows = maximumGrid.x - minimumGrid.x;
+		var minHeight = rotatedBounds.Mins.z;
+		var maxHeight = rotatedBounds.Maxs.z;
+
+		await GameTask.RunInThreadAsync( () =>
+		{
+			Log.Info( $"{(Game.IsServer ? "[Server]" : "[Client]")} Casting {(maximumGrid.y - minimumGrid.y) * (maximumGrid.x - minimumGrid.x)} cells. [{maximumGrid.x - minimumGrid.x}x{maximumGrid.y - minimumGrid.y}]" );
+
+			for ( int column = 0; column < totalColumns; column++ )
+			{
+				for ( int row = 0; row < totalRows; row++ )
+				{
+					var startPosition = worldBounds.Mins.WithZ( worldBounds.Maxs.z ) + new Vector3( row * currentGrid.CellSize + currentGrid.CellSize / 2f, column * currentGrid.CellSize + currentGrid.CellSize / 2f, currentGrid.Tolerance * 2f ) * currentGrid.AxisRotation;
+					var endPosition = worldBounds.Mins + new Vector3( row * currentGrid.CellSize + currentGrid.CellSize / 2f, column * currentGrid.CellSize + currentGrid.CellSize / 2f, -currentGrid.Tolerance ) * currentGrid.AxisRotation;
+					var checkBBox = new BBox( new Vector3( -currentGrid.CellSize / 2f + currentGrid.Tolerance, -currentGrid.CellSize / 2f + currentGrid.Tolerance, 0f ), new Vector3( currentGrid.CellSize / 2f - currentGrid.Tolerance, currentGrid.CellSize / 2f - currentGrid.Tolerance, 0.001f ) );
+					var positionTrace = Sandbox.Trace.Box( checkBBox, startPosition, endPosition )
+					.WithAllTags( currentGrid.Settings.TagsToInclude.ToArray() )
+					.WithoutTags( currentGrid.Settings.TagsToExclude.ToArray() );
+
+					if ( currentGrid.WorldOnly )
+						positionTrace.WorldOnly();
+					else
+						positionTrace.WorldAndEntities();
+
+					var positionResult = positionTrace.Run();
+
+					while ( positionResult.Hit && startPosition.z >= endPosition.z )
+					{
+						if ( currentGrid.IsInsideBounds( positionResult.HitPosition ) )
+						{
+							if ( !currentGrid.CylinderShaped || currentGrid.IsInsideCylinder( positionResult.HitPosition ) )
+							{
+								var angle = Vector3.GetAngle( Vector3.Up, positionResult.Normal );
+								if ( angle <= currentGrid.StandableAngle )
+								{
+									var newCell = Cell.TryCreate( currentGrid, positionResult.HitPosition );
+
+									if ( newCell != null )
+										currentGrid.AddCell( newCell );
+								}
+							}
+						}
+
+						startPosition = positionResult.HitPosition + Vector3.Down * currentGrid.HeightClearance;
+
+						while ( Sandbox.Trace.TestPoint( startPosition, radius: currentGrid.CellSize / 2f - currentGrid.Tolerance ) )
+							startPosition += Vector3.Down * currentGrid.HeightClearance;
+
+						positionTrace = Sandbox.Trace.Box( checkBBox, startPosition, endPosition )
+						.WithAllTags( currentGrid.Settings.TagsToInclude.ToArray() )
+						.WithoutTags( currentGrid.Settings.TagsToExclude.ToArray() );
+
+						if ( currentGrid.WorldOnly )
+							positionTrace.WorldOnly();
+						else
+							positionTrace.WorldAndEntities();
+
+						positionResult = positionTrace.Run();
+					}
+				}
+			}
+		} );
+
+		Stopwatch edgeCells = new Stopwatch();
+		edgeCells.Start();
+		currentGrid.AssignEdgeCells();
+		edgeCells.Stop();
+		Log.Info( $"{(Game.IsServer ? "[Server]" : "[Client]")} Grid {currentGrid.Identifier} assigned edge cells in {edgeCells.ElapsedMilliseconds}ms" );
+
+		totalWatch.Stop();
+		Log.Info( $"{(Game.IsServer ? "[Server]" : "[Client]")} Grid {currentGrid.Identifier} created in {totalWatch.ElapsedMilliseconds}ms" );
+
+		currentGrid.Initialize();
+
+		return currentGrid;
+	}
+
+	public override int GetHashCode()
+	{
+		var identifierHashCode = Identifier.GetHashCode();
+		var positionHashCode = Position.GetHashCode();
+		var boundsHashCode = Bounds.GetHashCode();
+		var rotationHashCode = Rotation.GetHashCode();
+		var axisAlignedHashCode = AxisAligned.GetHashCode();
+		var standableAngleHashCode = StandableAngle.GetHashCode();
+		var stepSizeHashCode = StepSize.GetHashCode();
+		var cellSizeHashCode = CellSize.GetHashCode();
+		var heightClearanceHashCode = HeightClearance.GetHashCode();
+		var widthClearanceHashCode = WidthClearance.GetHashCode();
+		var gridPerfectHashCode = GridPerfect.GetHashCode();
+		var worldOnlyHashCode = WorldOnly.GetHashCode();
+
+		var hashCodeFirst = HashCode.Combine( identifierHashCode, positionHashCode, boundsHashCode, rotationHashCode, axisAlignedHashCode, standableAngleHashCode, stepSizeHashCode, cellSizeHashCode );
+		var hashCodeSecond = HashCode.Combine( cellSizeHashCode, heightClearanceHashCode, widthClearanceHashCode, gridPerfectHashCode, worldOnlyHashCode );
+
+		return HashCode.Combine( hashCodeFirst, hashCodeSecond );
+	}
+
+}
