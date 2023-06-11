517bc50de5ebea25be6fd6322f56f53917bbc2a9
yuberee <59583743+yuberee@users.noreply.github.com>
2023-06-11 11:53:08 +0200

    IntVector3 + and - operators

--- Grid-Astar-Library/code/Utility/IntVector2.cs
+++ Grid-Astar-Library/code/Utility/IntVector2.cs
@@ -41,53 +41,17 @@ public struct IntVector2 : IEquatable<IntVector2>
 		this.y = y;
 	}
 
-	public IntVector2 WithX( int x )
-	{
-		return new IntVector2( x, this.y );
-	}
+	public IntVector2 WithX( int x ) => new IntVector2( x, this.y );
+	public IntVector2 WithY( int y ) => new IntVector2( this.x, y );
+	public Vector2 ToVector2() => new Vector2( x, y );
+	public float DistanceSquared( IntVector2 other ) => ToVector2().DistanceSquared( other.ToVector2());
+	public override string ToString() => $"{x},{y}";
+	public override bool Equals( object obj ) => obj is IntVector2 other && Equals( other );
+	public bool Equals( IntVector2 other ) => x == other.x && y == other.y;
+	public override int GetHashCode() => HashCode.Combine( x, y );
 
-	public IntVector2 WithY( int y )
-	{
-		return new IntVector2( this.x, y );
-	}
-
-	public Vector2 ToVector2()
-	{
-		return new Vector2( x, y );
-	}
-
-	public float DistanceSquared( IntVector2 other )
-	{
-		return ToVector2().DistanceSquared( other.ToVector2());
-	}
-
-	public override string ToString()
-	{
-		return $"{x},{y}";
-	}
-
-	public override bool Equals( object obj )
-	{
-		return obj is IntVector2 other && Equals( other );
-	}
-
-	public bool Equals( IntVector2 other )
-	{
-		return x == other.x && y == other.y;
-	}
-
-	public override int GetHashCode()
-	{
-		return HashCode.Combine( x, y );
-	}
-
-	public static bool operator ==( IntVector2 left, IntVector2 right )
-	{
-		return left.Equals( right );
-	}
-
-	public static bool operator !=( IntVector2 left, IntVector2 right )
-	{
-		return !(left == right);
-	}
+	public static bool operator ==( IntVector2 left, IntVector2 right ) => left.Equals( right );
+	public static bool operator !=( IntVector2 left, IntVector2 right ) => !(left == right);
+	public static IntVector2 operator +( IntVector2 a, IntVector2 b ) => new IntVector2( a.x + b.x, a.y + b.y );
+	public static IntVector2 operator -( IntVector2 a, IntVector2 b ) => new IntVector2( a.x - b.x, a.y - b.y );
 }
